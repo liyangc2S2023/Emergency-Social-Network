51b52fc9e144339fcfff2e44a1f673a32aab6cca
Rui Chen <rchen4@andrew.cmu.edu>
2023-02-07 00:39:36 -0800

    add password encrypt

--- service/joinService.js
+++ service/joinService.js
@@ -1,6 +1,7 @@
 const User = require("../model/user")
 const jwt = require('jsonwebtoken')
 const config = require('../config')
+const authentication = require("../model/authentication")
 
 class JoinService{
     async join(res,username,password){
@@ -9,7 +10,7 @@ class JoinService{
             username:username
         },config.JWT_KEY, {expiresIn:'1d'})
         // todo: learn more about promise
-        await User.create({"username":username,"password":password})
+        await User.create({"username":username,"password":authentication.encrypt(password)})
         res.status(200)
         res.cookie('user_token',token)
         res.render('rules');
