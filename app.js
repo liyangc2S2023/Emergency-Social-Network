0fa98ca1b42caa14d0e992b5715fa84956dfda4d
lisa <liyangc2@andrew.cmu.edu>
2023-02-07 16:40:06 -0800

    welcome rules ui v2

--- app.js
+++ app.js
@@ -30,21 +30,21 @@ app.use("/join", require("./routes/joinRouter"));
 app.use("/welcome", require("./routes/welcomeRouter"));
 
 // Middleware: JWT(Json Web Token) Authentication
-// app.use(function(req,res,next){
-//     const token = req.body.token || req.query.token || req.headers.authorization || req.cookies['user_token']
-//     if(!token){
-//         next(createError(401,"token required"))
-//     }
-//     else{
-//         try{
-//             const decoded=jwt.verify(token,config.JWT_KEY)
-//             req.username=decoded.username
-//             next()
-//         } catch(err){
-//             next(createError(401,"token invalid"+err.toString()))        
-//         }
-//     }
-// })
+app.use(function(req,res,next){
+    const token = req.body.token || req.query.token || req.headers.authorization || req.cookies['user_token']
+    if(!token){
+        next(createError(401,"token required"))
+    }
+    else{
+        try{
+            const decoded=jwt.verify(token,config.JWT_KEY)
+            req.username=decoded.username
+            next()
+        } catch(err){
+            next(createError(401,"token invalid"+err.toString()))        
+        }
+    }
+})
 
 app.use("/rules", require("./routes/welcomeRulesRouter"));
 
