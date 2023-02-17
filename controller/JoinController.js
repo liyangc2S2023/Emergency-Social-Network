fcd1e7961303826f044396951ddafb2308045100
Lin Xi <yxiang2@andrew.cmu.edu>
2023-02-17 12:02:02 -0800

    hint functions to login logout

--- controller/JoinController.js
+++ controller/JoinController.js
@@ -1,6 +1,27 @@
 const User = require("../model/user")
 
 class JoinController{
+
+    async login(username, password) {
+        //...
+        var {successflag, joinErr} = User.nameRuleCheck(username, password)
+        if (!successflag) {
+            return {successflag, joinErr}
+        }
+
+        // check name exist
+        var {successflag, userObject} = User.usernameExists(username)
+
+        // user not exist
+        if (!successflag) {
+            // join function
+        } else {
+            // verify user password
+        }
+
+        return {successflag, userObject}
+
+    }
  
     async join(username, password) {
         // apply basic rule check when people hit join button.
