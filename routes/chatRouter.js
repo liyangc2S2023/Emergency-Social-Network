626e8c5aef9d26d62399e9eb2e0904f0643bcb97
Rui Chen <rchen4@andrew.cmu.edu>
2023-02-28 04:22:24 -0800

    pass lint

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
 const renderOnePage = require('./common/renderOnePage');
 
-router.get('/', async (req, res) => {
-  return renderOnePage(req,res,'Public')
-});
+router.get('/', async (req, res) => renderOnePage(req, res, 'Public'));
 
 module.exports = router;
--- routes/directoryRouter.js
+++ routes/directoryRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
 const renderOnePage = require('./common/renderOnePage');
 
-router.get('/', async (req, res,next) => {
-  return renderOnePage(req,res,'Directory')
-});
+router.get('/', async (req, res) => renderOnePage(req, res, 'Directory'));
 
 module.exports = router;
