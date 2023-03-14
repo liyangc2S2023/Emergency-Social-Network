6479c1971c1625aba8af697c180324dcc7c7905d
Rui Chen <rchen4@andrew.cmu.edu>
2023-03-13 19:08:11 -0700

    remove test

--- middleware/speedTestMW.js
+++ middleware/speedTestMW.js
@@ -3,9 +3,7 @@ const SuspendFlag = require('../routes/common/suspendFlag');
 
 module.exports = (req, res, next) => {
   if (SuspendFlag.getInstance().isSuspend
-      && req.headers.testid !== SuspendFlag.getInstance().testID
-      // for test only
-      && req.headers.testid !== 'cradmin-1678588926922') {
+      && req.headers.testid !== SuspendFlag.getInstance().testID) {
     next(createHttpError(503, 'Website is temporarily suspended'));
   } else {
     next();
