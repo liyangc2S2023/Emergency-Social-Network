23492c0d8c270ef9e11201f46228639b9dade245
Lin Xi <yxiang2@andrew.cmu.edu>
2023-03-14 17:06:20 -0700

    update status on the status page

--- public/javascript/status.js
+++ public/javascript/status.js
@@ -4,14 +4,16 @@ $(document).ready(function () {
 
 function changeStatus() {
   const username = $('#username').val();
-  var newStatus = $('input[name="status"]').val();
+  const newStatus = $('input[name="status"]:checked').val();
   console.log('change status to: ' + newStatus, ' for user: ' + username)
+  let updatedStatus;
   axios.post('/api/v1/status', {
     "username": username,
     "status": newStatus
   }).then(function (res) {
     console.log(res);
     updatedStatus = res.data.data.status;
-    $('#status').val(updatedStatus);
+    console.log(updatedStatus);
+    $('#userStatus').text(updatedStatus);
   });
 }
