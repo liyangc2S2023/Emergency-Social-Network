2b32b2ca2d45eb919e9cb6a9d49b7ca70f66699f
Rui Chen <rchen4@andrew.cmu.edu>
2023-03-14 20:58:50 -0700

    fix bugs

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -15,8 +15,8 @@ function oneKeyPress(e) {
 
 function sendPublicMessage() {
   const inputContent = $('#inputContent').val();
-  const username = $('#username').val();
-  const status = $('#status').val();
+  const username = $('#currentUsername').val();
+  const status = $('#currentUserStatus').val();
   if (inputContent == '' || username == '' || status == '') {
     alert(`input text:${inputContent} or username:${username} or user status:${status} cannot be null`);
   } else {
@@ -40,8 +40,8 @@ $(document).ready(() => {
   axios.get('api/v1/users/current').then((res) => {
     const { username } = res.data.data;
     const { status } = res.data.data;
-    $('#username').val(username);
-    $('#status').val(status);
+    $('#currentUsername').val(username);
+    $('#currentUserStatus').val(status);
   }).catch((err) => {
     alert(err);
   });
