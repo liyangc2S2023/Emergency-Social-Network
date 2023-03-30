08750fe0f0c85b1908bd1cbddb99d7688e444c17
Rui Chen <rchen4@andrew.cmu.edu>
2023-03-29 19:21:23 -0700

    extend axios timeout, fix speed test

--- controller/speedController.js
+++ controller/speedController.js
@@ -1,21 +1,26 @@
 const config = require('../config');
-const SpeedRecord = require('../model/speedRecord');
 const SuspendFlag = require('../routes/common/suspendFlag');
+const DB = require('../database');
 
 class SpeedRecordController {
+  testDB;
   static async startTest(testID) {
     // set suspend flag
     await SuspendFlag.getInstance().startSuspend(testID);
     // set test database
-    await SpeedRecord.getInstance().setTestDB();
+    console.log("start speed test: "+testID)
+    await new DB('production').disconnect();
+    this.testDB = new DB('test');
+    await this.testDB.connect();
   }
 
   static async stopTest() {
-    // reset database
-    setTimeout(
-      () => { SpeedRecord.getInstance().resetDB(); },
-      config.SPEED_TEST_WAIT_TIME,
-    );
+    await new Promise((resolve) => {
+      setTimeout(resolve,config.SPEED_TEST_WAIT_TIME);
+    }).then(async()=>{
+      await this.testDB.disconnect();
+      await new DB('production').connect();
+    })
   }
 }
 
